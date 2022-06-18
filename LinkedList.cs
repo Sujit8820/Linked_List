1a6592dcf3c74f3f840832fe64aaa6c35d33bcbe
Sujit8820 <kadamsujit8820@gmail.com>
2022-06-18 16:22:18 +0530

    [Sujit]Add: Node on Top

---
+++ LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_New
+{
+    public class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public void AddNode(Node node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("Linked list is empty");
+            else
+                Console.WriteLine("Linked List: ");
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                    Console.Write("==>");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_New
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcom to LinkedList Program");
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+            LinkedList list = new LinkedList();
+            list.AddNode(node1);
+            list.AddNode(node2);
+            list.AddNode(node3);
+            list.Display();
+        }
+
+
+    }
+
+}
